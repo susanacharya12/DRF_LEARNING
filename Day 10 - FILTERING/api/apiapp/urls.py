b948c9bc1e7ae1353e0d076dd1189f1981c28005
Susan Acharya <susanacharya.sp@gmail.com>
2025-08-13 18:53:30 +0545

    Add files via upload

---
+++ Day 10 - FILTERING/api/apiapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import StudentList
+
+urlpatterns = [
+   path('api/', StudentList.as_view(), name='api')
+]
---
+++ Day 10 - FILTERING/api/apiapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializers import StudentSerializer
+from .models import Student
+
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    filterset_fields = ['name', 'city']
+    
+  

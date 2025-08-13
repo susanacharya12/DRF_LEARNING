51d46ff2feceb3e931c2085b8a0b6998240bf57a
Susan Acharya <susanacharya.sp@gmail.com>
2025-08-13 19:22:24 +0545

    Add files via upload

---
+++ Day13-PAGINATION/page/pageapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.generics import ListAPIView
+from .mypagination import PageNumberPagination
+
+
+# Create your views here.
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    pagination_class =PageNumberPagination
+    

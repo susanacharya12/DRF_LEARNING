447e98041e4900a8971b818b54745d37510d035c
Susan Acharya <susanacharya.sp@gmail.com>
2025-08-19 21:52:40 +0545

    Add files via upload

---
+++ Day 15-TOKEN AUTHENTICATION/student_api/student_api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('students.urls')),
+    path('gettoken/', obtain_auth_token),
+]
